d1ad5498fa2420238c942b9fd6d1714d96f3c578
Marcos Bonatto <mbonatto52@gmail.com>
2015-04-09 14:54:23 -0300

    retornos de erro

--- src/mthread.c
+++ src/mthread.c
@@ -1,18 +1,18 @@
 int mcreate (int prio, void (*start)(void*), void *arg) {
-	
+	return -1; // ERROU
 }
 int myield(void) {
-	
+	return -1; // ERROU
 }
 int mwait(int tid) {
-	
+	return -1; // ERROU
 }
 int mmutex_init(mmutex_t *mtx) {
-	
+	return -1; // ERROU
 }
 int mlock (mmutex_t *mtx) {
-	
+	return -1; // ERROU
 }
 int munlock (mmutex_t *mtx) {
-	
+	return -1; // ERROU
 }
