0f414650bda7193a7d52296df137727f90b63c6c
Aleuck <aleuck@github.com>
2015-05-04 00:12:49 -0300

    mcreate done

--- Makefile
+++ Makefile
@@ -15,10 +15,10 @@ INC_DIR=./include
 BIN_DIR=./bin
 SRC_DIR=./src
 
-all: regra1 regra2 regran
+all: mthread.o regra2 regran
 
-regra1: #dependências para a regra1
-	$(CC) -o $(BIN_DIR)regra1 $(SRC_DIR)regra1.c -Wall
+mthread.o: #dependências para a regra1
+	$(CC) -o $(BIN_DIR)/mthread.o -c $(SRC_DIR)/mthread.c -Wall
 
 regra2: #dependências para a regra2
 	$(CC) -o $(BIN_DIR)regra2 $(SRC_DIR)regra2.c -Wall
--- src/mthread.c
+++ src/mthread.c
@@ -1,10 +1,80 @@
+#include <stdlib.h>
+#include <ucontext.h>
+#include "../include/mthread.h"
+#include "../include/mdata.h"
+
+// PRIVATE CONSTANTS
+#define PRIORITY_HIGH 0
+#define PRIORITY_MEDIUM 1
+#define PRIORITY_LOW 2
+#define STATE_CREATION 0
+#define STATE_READY 1
+#define STATE_EXECUTION 2
+#define STATE_BLOCKED 3
+
+// PRIVATE STRUCTURES
+typedef struct TCPqueue {
+	TCB_t *first, *last;
+} TCB_queue;
+
+
+// PRIVATE GLOBAL VARIABLES
+TCB_t main_thread = (struct TCB) { 0, 2, -1, {0}, NULL, NULL };
+TCB_t *execution = &main_thread;
+TCB_queue mqueues[4] = { { NULL, NULL }, { NULL, NULL }, { NULL, NULL }, { NULL, NULL } };
+
+
+// PRIVATE FUNCTIONS
+
+int add_to_queue(TCB_t *thread) {
+	TCB_queue *queue;
+	if (thread->prio < 0 || thread->prio > 2) {
+		return -1;
+	}
+	queue = &mqueues[thread->prio];
+	thread->prev = NULL;
+	if (queue->last != NULL) {
+		thread->next = queue->last;
+		queue->last->prev = thread;
+		queue->last = thread;
+	} else {
+		thread->next = NULL;
+		queue->last = thread;
+		queue->first = thread;
+	}
+	thread->state = 1;
+	return 0;
+}
+
+
+// PUBLIC FUNCTIONS
+
 int mcreate (int prio, void (*start)(void*), void *arg) {
-	return -1; // ERROU
+	static unsigned int last_tid = 0;
+	TCB_t* thread;
+
+	if (prio < 0 || prio > 2)
+		return -1; // ERROU
+
+	thread = malloc(sizeof(TCB_t));
+	if (thread == NULL) {
+		// could not allocate thread;
+		return -1;
+	}
+	thread->tid   = ++last_tid;
+	thread->state = STATE_CREATION;
+	thread->prio  = prio;
+	//getcontext(&(thread->context));
+	makecontext(&(thread->context), start, 1, arg);
+	return add_to_queue(thread);
 }
 int myield(void) {
+	add_to_queue(execution);
+	//TODO: EXECUTE NEXT
 	return -1; // ERROU
 }
 int mwait(int tid) {
+	execution->state = STATE_BLOCKED;
 	return -1; // ERROU
 }
 int mmutex_init(mmutex_t *mtx) {
